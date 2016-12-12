f96e40d603b274e8262ff762f5046b8e5fab28b9
Lisa Holter <lisaholter@Lisas-MacBook-Air.local>
2016-12-12 16:10:43 -0500

    auth finish

--- models/found.js
+++ models/found.js
@@ -8,4 +8,20 @@ var FoundObjectSchema = new mongoose.Schema({
   timestamps: true
 });
 
+function date2String(date) {
+  var options = {
+    weekday: 'long', year: 'numeric', month: 'short',
+    day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'
+  };
+  return date.toLocaleDateString('en-US', options);
+}
+
+FoundObjectSchema.methods.getCreatedAt = function() {
+  return date2String(this.createdAt);
+};
+
+FoundObjectSchema.methods.getUpdatedAt = function() {
+  return date2String(this.updatedAt);
+};
+
 module.exports = mongoose.model('FoundObject', FoundObjectSchema);
--- routes/found-objects.js
+++ routes/found-objects.js
@@ -9,47 +9,80 @@ function makeError(res, message, status) {
   return error;
 }
 
+function authenticate(req, res, next) {
+  if(!req.isAuthenticated()) {
+    req.flash('error', 'Please signup or login.');
+    res.redirect('/');
+  }
+  else {
+    next();
+  }
+}
+
 // INDEX
-router.get('/', function(req, res, next) {
+router.get('/', authenticate, function(req, res, next) {
   // get all the todos and render the index view
-  FoundObject.find({})
-  .then(function(foundObjects) {
-    res.render('found-objects/index', { foundObjects: foundObjects } );
+  var foundObjects = global.currentUser.foundObjects;
+    res.render('found-objects/index', { foundObjects: foundObjects, message: req.flash() } );
   }, function(err) {
     return next(err);
   });
-});
 
 // NEW
-router.get('/new', function(req, res, next) {
+router.get('/new', authenticate, function(req, res, next) {
   var foundObject = {
     title: '',
     location: '',
     about: '',
+    timestamp: true
   };
   res.render('found-objects/new', { foundObject: foundObject } );
 });
 
+
 // SHOW
-router.get('/:id', function(req, res, next) {
-  FoundObject.findById(req.params.id)
-  .then(function(foundObject) {
-    if (!foundObject) return next(makeError(res, 'Document not found', 404));
-    res.render('found-objects/show', { foundObject: foundObject });
-  }, function(err) {
-    return next(err);
-  });
+// router.get('/:id', authenticate, function(req, res, next) {
+//   FoundObject.findById(req.params.id)
+//   .then(function(foundObject) {
+//     if (!foundObject) return next(makeError(res, 'Document not found', 404));
+//     res.render('found-objects/show', { foundObject: foundObject });
+//   }, function(err) {
+//     return next(err);
+//   });
+// });
+
+router.get('/:id', authenticate, function(req, res, next) {
+  var foundObject = currentUser.foundObjects.id(req.params.id);
+  if (!foundObject) return next(makeError(res, 'Document not found', 404));
+  res.render('found-objects/show', { foundObject: foundObject, message: req.flash() } );
 });
 
 // CREATE
-router.post('/', function(req, res, next) {
-  var foundObject = new FoundObject({
+// router.post('/', function(req, res, next) {
+//   var foundObject = new FoundObject({
+//     title: req.body.title,
+//     location: req.body.location,
+//     about: req.body.about
+//   });
+//   foundObject.save()
+//   .then(function(saved) {
+//     res.redirect('/found-objects');
+//   }, function(err) {
+//     return next(err);
+//   });
+// });
+
+router.post('/', authenticate, function(req, res, next) {
+  var foundObject = {
     title: req.body.title,
     location: req.body.location,
-    about: req.body.about,
-  });
-  foundObject.save()
-  .then(function(saved) {
+    about: req.body.about
+  };
+  // Since a user's todos are an embedded document, we just need to push a new
+  // TODO to the user's list of todos and save the user.
+  currentUser.foundObjects.push(foundObject);
+  currentUser.save()
+  .then(function() {
     res.redirect('/found-objects');
   }, function(err) {
     return next(err);
@@ -57,37 +90,72 @@ router.post('/', function(req, res, next) {
 });
 
 // EDIT
-router.get('/:id/edit', function(req, res, next) {
-  FoundObject.findById(req.params.id)
-  .then(function(foundObject) {
-    if (!foundObject) return next(makeError(res, 'Document not found', 404));
-    res.render('found-objects/edit', { foundObject: foundObject });
-  }, function(err) {
-    return next(err);
-  });
+// router.get('/:id/edit', function(req, res, next) {
+//   FoundObject.findById(req.params.id)
+//   .then(function(foundObject) {
+//     if (!foundObject) return next(makeError(res, 'Document not found', 404));
+//     res.render('found-objects/edit', { foundObject: foundObject });
+//   }, function(err) {
+//     return next(err);
+//   });
+// });
+
+router.get('/:id/edit', authenticate, function(req, res, next) {
+  var foundObject = currentUser.foundObjects.id(req.params.id);
+  if (!foundObject) return next(makeError(res, 'Document not found', 404));
+  res.render('found-objects/edit', { foundObject: foundObject, message: req.flash() } );
 });
 
 // UPDATE
-router.put('/:id', function(req, res, next) {
-  FoundObject.findById(req.params.id)
-  .then(function(foundObject) {
-    if (!foundObject) return next(makeError(res, 'Document not found', 404));
+// router.put('/:id', function(req, res, next) {
+//   FoundObject.findById(req.params.id)
+//   .then(function(foundObject) {
+//     if (!foundObject) return next(makeError(res, 'Document not found', 404));
+//     foundObject.title = req.body.title;
+//     foundObject.location = req.body.location;
+//     foundObject.about = req.body.about;
+//     return foundObject.save();
+//   })
+//   .then(function(saved) {
+//     res.redirect('/found-objects');
+//   }, function(err) {
+//     return next(err);
+//   });
+// });
+
+router.put('/:id', authenticate, function(req, res, next) {
+  var foundObject = currentUser.foundObjects.id(req.params.id);
+  if (!foundObject) return next(makeError(res, 'Document not found', 404));
+  else {
     foundObject.title = req.body.title;
     foundObject.location = req.body.location;
     foundObject.about = req.body.about;
-    return foundObject.save();
-  })
-  .then(function(saved) {
-    res.redirect('/found-objects');
-  }, function(err) {
-    return next(err);
-  });
+    currentUser.save()
+    .then(function(saved) {
+      res.redirect('/found-objects');
+    }, function(err) {
+      return next(err);
+    });
+  }
 });
 
 // DESTROY
-router.delete('/:id', function(req, res, next) {
-  FoundObject.findByIdAndRemove(req.params.id)
-  .then(function() {
+// router.delete('/:id', function(req, res, next) {
+//   FoundObject.findByIdAndRemove(req.params.id)
+//   .then(function() {
+//     res.redirect('/found-objects');
+//   }, function(err) {
+//     return next(err);
+//   });
+// });
+
+router.delete('/:id', authenticate, function(req, res, next) {
+  var foundObject = currentUser.foundObjects.id(req.params.id);
+  if (!foundObject) return next(makeError(res, 'Document not found', 404));
+  var index = currentUser.foundObjects.indexOf(foundObject);
+  currentUser.foundObjects.splice(index, 1);
+  currentUser.save()
+  .then(function(saved) {
     res.redirect('/found-objects');
   }, function(err) {
     return next(err);
