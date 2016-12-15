5fab59233e44bb3885a1f327ba1113bb74c2aa37
Lisa Holter <lisaholter@Lisas-MacBook-Air.local>
2016-12-15 11:26:08 -0500

    db connection

--- app.js
+++ app.js
@@ -20,7 +20,22 @@ var foundRouter = require('./routes/found-objects');
 var app = express();
 
 // Connect to database
-mongoose.connect('mongodb://localhost/foundApp');
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/foundApp');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
